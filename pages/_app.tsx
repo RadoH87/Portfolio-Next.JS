30b71ae348a52da57600a214f2999658bf09e08c
Radek Holubowski <radekh8787@gmail.com>
2022-02-28 20:49:35 +0000

    Add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-12 gap-6 my-14 px-5 lg:px-48">
+      <div className="col-span-12 p-4 text-center lg:col-span-3 rounded-2xl bg-emerald-400">
+        Sidebar
+      </div>
+      <div className="col-span-12 lg:col-span-9 rounded-2xl bg-emerald-400">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
