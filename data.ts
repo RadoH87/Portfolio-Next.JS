307fd101e1a712826d93014c75d84efc609c52c4
Radek Holubowski <radekh8787@gmail.com>
2022-03-06 18:29:41 +0000

    Add stages data & react icons

--- data.ts
+++ data.ts
@@ -1,8 +1,10 @@
 import { RiComputerLine } from "react-icons/ri";
-import { Service } from "./types";
+import { Service, Stage } from "./types";
 import { AiOutlineAntDesign, AiOutlineApi } from "react-icons/ai";
 import { MdDeveloperMode } from "react-icons/md";
-import { FaServer } from "react-icons/fa";
+import { FaServer, FaDatabase } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiNodedotjs, SiExpress, SiTypescript } from "react-icons/si";
 
 export const services: Service[] = [
   {
@@ -45,5 +47,56 @@ export const services: Service[] = [
     description:
       "Lorem ipsum dolor sit amet, consectetur sfdsdfdsfds sdfdsfdsfdsfds dsfdsfdsf",
     id: 6,
+  },
+];
+
+export const stages: Stage[] = [
+  {
+    Icon: IoLogoJavascript,
+    title: "Javascript",
+    date: "28.06 - 01.08.2021",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elitCupiditate ex labore maiores provident sit soluta veniam Aliquamat cumque ea ex exercitationem ipsa molestiae placeat quae quaeratrepudiandae similique totam ",
+    id: 1,
+  },
+  {
+    Icon: SiNodedotjs,
+    title: "NodeJS",
+    date: "16.08 - 19.09.2021",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elitCupiditate ex labore maiores provident sit soluta veniam Aliquamat cumque ea ex exercitationem ipsa molestiae placeat quae quaeratrepudiandae similique totam ",
+    id: 2,
+  },
+  {
+    Icon: SiExpress,
+    title: "Express",
+    date: "04.10 - 07.11.2021",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elitCupiditate ex labore maiores provident sit soluta veniam Aliquamat cumque ea ex exercitationem ipsa molestiae placeat quae quaeratrepudiandae similique totam ",
+    id: 3,
+  },
+  {
+    Icon: FaDatabase,
+    title: "MySQL & MongoDB",
+    date: "22.11 - 26.12.2021",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elitCupiditate ex labore maiores provident sit soluta veniam Aliquamat cumque ea ex exercitationem ipsa molestiae placeat quae quaeratrepudiandae similique totam ",
+    id: 4,
+  },
+  {
+    Icon: SiTypescript,
+    title: "TypeScript",
+    date: "10.01 - 13.02.2022",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elitCupiditate ex labore maiores provident sit soluta veniam Aliquamat cumque ea ex exercitationem ipsa molestiae placeat quae quaeratrepudiandae similique totam ",
+    id: 5,
+  },
+  {
+    Icon: SiTypescript,
+    title: "TypeScript",
+    date: "10.01 - 13.02.2022",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elitCupiditate ex labore maiores provident sit soluta veniam Aliquamat cumque ea ex exercitationem ipsa molestiae placeat quae quaeratrepudiandae similique totam ",
+    id: 5,
   },
 ];
