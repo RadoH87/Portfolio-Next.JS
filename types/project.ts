b2c2b41b1678bf329df20d8b8184a73b08740788
Radek Holubowski <radekh8787@gmail.com>
2022-03-12 21:52:17 +0000

    Add example images

--- types/project.ts
+++ types/project.ts
@@ -2,7 +2,7 @@ export interface Project {
   name: string;
   description: string;
   image_path: string;
-  deploy_url: string;
+  deployed_url: string;
   github_url: string;
   category: Category[];
   tech_stacks: string[];
