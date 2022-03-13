f04c3c7e2aa2540de9bfc99aa8e9eafc013b57b6
Radek Holubowski <radekh8787@gmail.com>
2022-03-13 15:10:04 +0000

    Add active item & styling

--- components/ProjectsNavbar.tsx
+++ components/ProjectsNavbar.tsx
@@ -1,30 +1,38 @@
 import { FC } from "react";
 import { Category } from "../types";
 
-export const NavItem: FC<{ value: Category | "all" }> = ({ value }) => {
+export const NavItem: FC<{
+  value: Category | "all";
+  handlerFilterCategory: Function;
+  active: string;
+}> = ({ value, handlerFilterCategory, active }) => {
+  let className =
+    "capitalize transition duration-100 transform cursor-pointer hover:scale-125 hover:text-indigo-500";
+  if (active === value) className += " text-indigo-500";
   return (
-    <li className="capitalize transition duration-100 transform cursor-pointer hover:scale-125 hover:text-indigo-500 ">
+    <li className={className} onClick={() => handlerFilterCategory(value)}>
       {value}
     </li>
   );
 };
 
-const ProjectsNavbar = () => {
-  return (
-    <div className="relative">
-      <div className="flex px-3 py-2 space-x-10 overflow-x-scroll list-none ">
-        <NavItem value="all" />
-        <NavItem value="javascript" />
-        <NavItem value="node" />
-        <NavItem value="express" />
-        <NavItem value="next" />
-        <NavItem value="nest" />
-        <NavItem value="mongoDB" />
-        <NavItem value="MYSQL" />
+const ProjectsNavbar: FC<{ handlerFilterCategory: Function; active: string }> =
+  (props) => {
+    return (
+      <div className="relative">
+        <div className="flex px-3 py-2 space-x-10 overflow-x-scroll list-none ">
+          <NavItem value="all" {...props} />
+          <NavItem value="javascript" {...props} />
+          <NavItem value="node" {...props} />
+          <NavItem value="express" {...props} />
+          <NavItem value="next" {...props} />
+          <NavItem value="nest" {...props} />
+          <NavItem value="mongoDB" {...props} />
+          <NavItem value="MYSQL" {...props} />
+        </div>
+        <div className="absolute top-0 right-0 w-1/12 h-10 dark:bg-gradient-to-l dark:from-[#0F1115] to-transparent" />
       </div>
-      <div className="absolute top-0 right-0 w-1/12 h-10 dark:bg-gradient-to-l dark:from-[#0F1115] to-transparent" />
-    </div>
-  );
-};
+    );
+  };
 
 export default ProjectsNavbar;
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import ProjectCard from "../components/ProjectCard";
 import ProjectsNavbar from "../components/ProjectsNavbar";
-import { projects } from "../data";
+import { projects as projectsData } from "../data";
+import { Category } from "../types";
 
 const Projects = () => {
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState("all");
+
+  const handlerFilterCategory = (category: Category | "all") => {
+    if (category === "all") {
+      setProjects(projectsData);
+      setActive(category);
+      return;
+    }
+    const newArray = projectsData.filter((project) =>
+      project.category.includes(category)
+    );
+    setProjects(newArray);
+    setActive(category);
+  };
+
   return (
     <div className="px-5 py-2 lg:overflow-y-scroll lg:h-[70vh]">
-      <ProjectsNavbar />
+      <ProjectsNavbar
+        handlerFilterCategory={handlerFilterCategory}
+        active={active}
+      />
       <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
           <ProjectCard {...project} key={project.id} />
